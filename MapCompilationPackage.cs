f7110202d3ce221976ef0b8a9622bb5f394b3238
Zakary Boven <zakaryboven@gmail.com>
2022-09-26 10:39:01 +1300

    Separate MapCompilationPackage into separate file

---
+++ MapCompilationPackage.cs
@@ -0,0 +1,22 @@
+﻿namespace AzerothWarsMapCompiler;
+
+public class MapCompilationPackage
+{
+  public MapCompilationPackage(string packageName, string sourceMapPath, string[] sourceCodeDirectoryPaths,
+    string[] objectDirectoryPaths, string fileName, string versionNumber)
+  {
+    PackageName = packageName;
+    SourceMapPath = sourceMapPath;
+    SourceCodeDirectoryPaths = sourceCodeDirectoryPaths;
+    ObjectDirectoryPaths = objectDirectoryPaths;
+    FileName = fileName;
+    VersionNumber = versionNumber;
+  }
+
+  public string PackageName { get; }
+  public string SourceMapPath { get; }
+  public string[] SourceCodeDirectoryPaths { get; }
+  public string[] ObjectDirectoryPaths { get; }
+  public string FileName { get; }
+  public string VersionNumber { get; }
+}
--- MapCompiler.cs
+++ MapCompiler.cs
@@ -40,24 +40,3 @@ public class MapCompiler
     if (launch) Wc3Wrapper.Run(launchPath);
   }
 }
-
-public class MapCompilationPackage
-{
-  public MapCompilationPackage(string packageName, string sourceMapPath, string[] sourceCodeDirectoryPaths,
-    string[] objectDirectoryPaths, string fileName, string versionNumber)
-  {
-    PackageName = packageName;
-    SourceMapPath = sourceMapPath;
-    SourceCodeDirectoryPaths = sourceCodeDirectoryPaths;
-    ObjectDirectoryPaths = objectDirectoryPaths;
-    FileName = fileName;
-    VersionNumber = versionNumber;
-  }
-
-  public string PackageName { get; }
-  public string SourceMapPath { get; }
-  public string[] SourceCodeDirectoryPaths { get; }
-  public string[] ObjectDirectoryPaths { get; }
-  public string FileName { get; }
-  public string VersionNumber { get; }
-}

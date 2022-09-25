b01050a9f99cba2721ab6c06bfd86dea205dabe2
Zakary Boven <zakaryboven@gmail.com>
2022-09-26 11:19:23 +1300

    Added extra test

--- test/AzerothWarsMapCompiler.Test/CommandLineUtilsTests.cs
+++ test/AzerothWarsMapCompiler.Test/CommandLineUtilsTests.cs
@@ -4,6 +4,7 @@ public class CommandLineUtilsTests
 {
   [Theory]
   [InlineData(@"C:\Program Files (x86)\Warcraft III\_retail_\x86_64", "\"C:\\Program Files (x86)\\Warcraft III\\_retail_\\x86_64\"")]
+  [InlineData(@"C:\Users\Bean Lord\OneDrive\Documents\Warcraft III\JassHelper", "\"C:\\Users\\Bean Lord\\OneDrive\\Documents\\Warcraft III\\JassHelper\"")]
   public void ParseCommand(string inputCommand, string expectedOutput)
   {
     var parsedCommand = CommandLineUtils.ParseCommand(inputCommand);
